e6e330a4b224d3ad8e31e77c02f5d6da68c36b93
Joshua Bon <joshuajbon@gmail.com>
2019-04-29 11:52:41 +1000

    Before adding c code

---
+++ R/smcdar.R
@@ -0,0 +1,20 @@
+#' \code{smcdar}: A package for delayed-acceptance SMC.
+#'
+#' smcdar...
+#'
+#' @section \code{smcdar} objects:
+#' \itemize{
+#'    \item \code{\link{particles}}
+#' }
+#' @section \code{smcdar} functions:
+#' \itemize{
+#'    \item \code{\link{mvn_jitter}}
+#'    \item \code{\link{num_particles}}
+#'    \item \code{\link{p_components}}
+#'    \item \code{\link{p_dim}}
+#'    \item \code{\link{p_shape}}
+#' }
+#'
+#' @docType package
+#' @name smcdar
+NULL
