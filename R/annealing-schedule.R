dcf1527f43acabba89defbed25cdd50df58c4350
Joshua Bon <joshuajbon@gmail.com>
2019-06-26 11:37:15 +1000

    Added memoise to annealed posteriors

--- R/annealing-schedule.R
+++ R/annealing-schedule.R
@@ -4,23 +4,42 @@
 #'
 #' @param log_posterior A function for the log-posterior.
 #' @param log_mu A function for the starting distribution.
+#' @param use_memoise Use memoise on posterior and mu functions?
 #'
 #' @return Function that outputs annealed log-likelihood.
 #' @export
-log_geometric_anneal_func <- function(log_posterior, log_mu){
+log_geometric_anneal_func <- function(log_posterior, log_mu, use_memoise = F){
 
   stopifnot(is.function(log_posterior),
             is.function(log_mu)
             )
 
-  function(x, temp, ...){
+  if(!use_memoise){
 
-    stopifnot(0 <= temp & temp <= 1)
+    f <- function(x, temp, ...){
 
-    temp * log_posterior(x, ...) + (1 - temp) * log_mu(x, ...)
+      stopifnot(0 <= temp & temp <= 1)
+
+      temp * log_posterior(x, ...) + (1 - temp) * log_mu(x, ...)
+
+    }
+
+  } else {
+
+    mem_log_posterior <- memoise::memoise(log_posterior)
+    mem_log_mu <- memoise::memoise(log_mu)
+
+    f <- function(x, temp, ...){
+
+      stopifnot(0 <= temp & temp <= 1)
+
+      temp * mem_log_posterior(x, ...) + (1 - temp) * mem_log_mu(x, ...)
+
+    }
 
   }
 
+  return(f)
 
 }
 
@@ -30,20 +49,41 @@ log_geometric_anneal_func <- function(log_posterior, log_mu){
 #'
 #' @param log_likelihood A function for the log-likelihood.
 #' @param log_prior A function for the starting distribution.
+#' @param use_memoise Use memoise on posterior and mu functions?
 #'
 #' @return Function that outputs annealed log-likelihood.
 #' @export
-log_likelihood_anneal_func <- function(log_likelihood, log_prior){
+log_likelihood_anneal_func <- function(log_likelihood, log_prior, use_memoise = F){
 
   stopifnot(is.function(log_likelihood),
             is.function(log_prior)
   )
 
-  function(x, temp, ...){
+  if(!use_memoise){
 
-    temp * log_likelihood(x, ...) +  log_prior(x, ...)
+    f <- function(x, temp, ...){
+
+      stopifnot(0 <= temp & temp <= 1)
+
+      temp * log_likelihood(x, ...) +  log_prior(x, ...)
+
+    }
+
+  } else {
+
+    mem_log_likelihood <- memoise::memoise(log_likelihood)
+    mem_log_prior <- memoise::memoise(log_prior)
+
+    f <- function(x, temp, ...){
+
+      stopifnot(0 <= temp & temp <= 1)
+
+      temp * mem_log_likelihood(x, ...) + mem_log_prior(x, ...)
+
+    }
 
   }
 
+  return(f)
 
 }
