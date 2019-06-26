02f927a98ed72b2e3f2ae2e29263e73777981c74
Joshua Bon <joshuajbon@gmail.com>
2019-06-26 11:37:25 +1000

    More work on standard SMC

--- analysis/lotka-volterra-ctmc-smc-da.R
+++ analysis/lotka-volterra-ctmc-smc-da.R
@@ -27,6 +27,8 @@
     geom_line(aes(x = time, y = level, colour = pop_name)) +
     theme_bw()
 
+  sim <- sim[sim[,"time"] < 30,]
+
 #### SMC ####
 
   #prior: theta ~ logNormal(-1,1) or log(theta) ~ Normal(-1,1)
@@ -56,22 +58,64 @@
   log_ann_post_ctmc <-
     log_likelihood_anneal_func(
       log_likelihood = log_like,
-      log_prior = log_prior
+      log_prior = log_prior,
+      use_memoise = T
       )
 
   draw_prior <- function(n){
     matrix(
-      rlnorm(n*3, meanlog = -2, sdlog = 0.5), ncol = 3
+      rlnorm(n*3, meanlog = c(-2,-5,-2), sdlog = 0.5), ncol = 3
     )
   }
 
   ## setup
-  num_p <- 100
+  num_p <- 20
+  curr_temp <- 0
 
-  # particles
+  # particles (must have support on )
   log_theta_start <- draw_prior(num_p)
+
+
   partl <- particles(theta = log_theta_start)
-  log_post <- papply(partl, fun = log_prior)
+  curr_log_post <- papply(partl, fun = log_prior) #temp = 0
+
+  for(i in 2:length(temps)){
+
+    ## new temp
+    # use ESS to find temp
+    half_ess_temp_obj <- function(temp) {
+      log_post <- papply(partl, fun = log_ann_post_ctmc, temp = temp)
+      log_post <- replace(x = log_post, is.na(log_post), -Inf)
+
+      partl_temp <- partl
+      weights(partl_temp, log = T) <- weights(partl_temp, log = T) + log_post - curr_log_post
+
+      res <- (ess(partl_temp) - num_p/2)^2
+
+      ifelse(is.finite(res), res, 1e08)
+
+    }
+
+    temp_optim <- optim(par = curr_temp + 0.1, fn = half_ess_temp_obj, lower = 0, upper = 1, method = "Brent")
+
+
+    new_temp <- temp_optim$par
+
+    # new log posterior
+    curr_log_post <- papply(partl, fun = log_ann_post_ctmc, temp = new_temp)
+    curr_log_post <- replace(x = curr_log_post, is.na(curr_log_post), -Inf)
+
+    # weight update
+    weights(partl, log = T) <- weights(partl, log = T) + curr_log_post - prev_log_post
+
+    # resample
+    logw <- weights(partl, log = T)
+    groups_logw <- cut(logw, breaks = quantile(logw, probs = seq(0,1, length.out = 5)), include.lowest = T)
+
+
+  }
+
+  #log_post <-
 
   ## start
 
