d4cb6573341b4a8a15ad2a253ca4e40949627257
Joshua Bon <joshuajbon@gmail.com>
2019-08-14 10:37:35 +1000

    Added computation time recording to papply

--- R/particle-apply.R
+++ R/particle-apply.R
@@ -4,13 +4,36 @@
 #'
 #' @param particles Particle object.
 #' @param fun Function to apply.
+#' @param comp_time Record time taken for comutation? Logical.
 #' @param ...
 #'
 #' @return Function evaluated at each row.
 #' @export
 #'
-papply <- function(particles, fun, ...){
+papply <- function(particles, fun, comp_time = F, ...){
 
-  apply(particles, MARGIN = 1, FUN = fun, ...)
+  if(!comp_time){
+
+    apply(particles, MARGIN = 1, FUN = fun, ...)
+
+  } else {
+
+    fun_t <- function(...){
+      tout <- system.time({
+        out <- fun(...)
+      })
+
+      attr(out, "t") <- tout["user.self"]
+      return(out)
+    }
+
+    out <- lapply(split(particles[], row(particles[])), fun_t, ...)
+
+    vals <- simplify2array(out)
+    attr(vals, "comptime") <- unname(sapply(out, attr, which = "t"))
+
+    vals
+
+  }
 
 }
