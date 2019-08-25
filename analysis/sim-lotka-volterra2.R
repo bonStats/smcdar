afb8288418b513e93baeae35a9067f80afc6e2b7
Joshua Bon <joshuajbon@gmail.com>
2019-08-26 08:48:22 +1000

    Normal-normal first commit

---
+++ analysis/sim-lotka-volterra2.R
@@ -0,0 +1,166 @@
+.libPaths(Sys.getenv("R_LIB_USER"))
+outdir <- Sys.getenv("OUT_DIRECTORY")
+
+devtools::install_github("bonStats/smcdar")
+
+#### load packages ####
+
+library(smcdar)
+library(dplyr)
+#library(tidyr)
+#library(ggplot2)
+library(future)
+library(future.apply)
+
+####
+
+#### Settings ####
+
+  ## Priors, likelihoods
+
+  # prior: theta ~ logNormal(-1,0.5) or log(theta) ~ Normal(-1,0.5)
+  log_prior <- function(log_theta){
+
+    sum( dnorm(log_theta, mean = c(-2,-5,-2), sd = rep(0.5, 3), log = T) )
+
+  }
+
+  log_like <- function(log_theta, sim_data){
+    log_lhood_lotka_volterra_ctmc(
+      theta = exp(log_theta),
+      y1 = sim_data[,"y1"], y2 = sim_data[,"y2"],
+      y1_max = 100, y2_max = 100,
+      times = sim_data[,"time"]
+    )
+  }
+
+  log_like_approx <- function(log_theta, sim_data){
+    log_lhood_lotka_volterra_lna(
+      theta = exp(log_theta),
+      y1 = sim_data[,"y1"], y2 = sim_data[,"y2"],
+      times = sim_data[,"time"]
+    )
+  }
+
+  # true_theta = (birth rate prey, death rate prey/ birth rate pred, death rate pred)
+  g_pars <- list(N = 100, N_approx = 20, true_theta = c(0.17,0.01,0.2),
+                 log_prior = log_prior, log_like = log_like, log_like_approx = log_like_approx
+                 )
+
+  sim_settings <- rep(list(list(f_pars = NULL, g_pars = g_pars)), 2)
+
+  sim_settings[[1]]$f_pars <- list(
+    num_p = 100,
+    step_scale_set = c(0.01, 0.05, 0.1, 0.2),
+    b_s_start = c(0,0,0,0,0,0),
+    refresh_ejd_threshold = 0.01
+  )
+
+  sim_settings[[2]]$f_pars <- list(
+    num_p = 200,
+    step_scale_set = c(0.01, 0.05, 0.1, 0.2),
+    b_s_start = c(0,0,0,0,0,0),
+    refresh_ejd_threshold = 0.01
+  )
+
+####
+
+
+
+#### Code for sim ####
+
+  run_sim <- function(ss){
+
+    ## simulate data
+
+    sim <- simulate_lotka_volterra_ctmc(
+      ss$g_pars$true_theta,
+      y1_min = 0,
+      y2_min = 0,
+      y1_max = 100,
+      y2_max = 100,
+      initial_y1 = 40,
+      initial_y2 = 25,
+      total_time = 50
+    )
+
+    sim_data <- sim[1:min(ss$g_pars$N, nrow(sim)),]
+
+    log_prior <- ss$g_pars$log_prior
+    log_like <- function(log_theta) ss$g_pars$log_like(log_theta, sim_data = sim_data)
+    log_like_approx <- function(log_theta) ss$g_pars$log_like_approx(log_theta, sim_data = sim_data[1:ss$g_pars$N_approx, ])
+
+    ## more code
+    source("lotka-volterra-ctmc-smc-da.R")
+
+    ## run
+    smc_da <- with(ss$f_pars, smc_lotka_volterra_da(
+      use_da = T, use_approx = F,
+      num_p = num_p,
+      step_scale_set = step_scale_set,
+      b_s_start = b_s_start,
+      refresh_ejd_threshold = refresh_ejd_threshold,
+      log_prior = log_prior,
+      log_like = log_like,
+      log_like_approx = log_like_approx,
+      sim_data = sim_data
+    )
+    )
+    
+    smc_full <- with(ss$f_pars, smc_lotka_volterra_da(
+      use_da = F, use_approx = F,
+      num_p = num_p,
+      step_scale_set = step_scale_set,
+      b_s_start = b_s_start,
+      refresh_ejd_threshold = refresh_ejd_threshold,
+      log_prior = log_prior,
+      log_like = log_like,
+      log_like_approx = log_like_approx,
+      sim_data = sim_data
+    )
+    )
+
+    smc_approx <- with(ss$f_pars, smc_lotka_volterra_da(
+      use_da = F, use_approx = T,
+      num_p = num_p,
+      step_scale_set = step_scale_set,
+      b_s_start = b_s_start,
+      refresh_ejd_threshold = refresh_ejd_threshold,
+      log_prior = log_prior,
+      log_like = log_like,
+      log_like_approx = log_like_approx,
+      sim_data = sim_data
+    )
+    )
+
+
+    return(
+      list(sim_settings = ss,
+           sim_data = sim_data,
+           smc_da = smc_da,
+           smc_full = smc_full,
+           smc_approx = smc_approx
+      )
+    )
+
+  }
+
+####
+
+#### Run sim ####
+
+  cat("future::availableCores():", future::availableCores(),"\n")
+  plan(multicore, workers = future::availableCores() )
+
+  for(i in seq_along(sim_settings)){
+
+    res <- future_replicate(n = 1,
+                                 run_sim(ss = sim_settings[[i]]),
+                                 simplify = F)
+
+    saveRDS(res, file.path(outdir, paste0("sim_res_",i,"_20190816.rds")))
+
+  }
+
+
+
---
+++ analysis/sim-nn-smc-da.R
@@ -0,0 +1,216 @@
+.libPaths(Sys.getenv("R_LIB_USER"))
+outdir <- Sys.getenv("OUT_DIRECTORY")
+
+devtools::install_github("bonStats/smcdar")
+
+#### load packages ####
+
+library(smcdar)
+library(dplyr)
+#library(tidyr)
+#library(ggplot2)
+library(future)
+#library(future.apply)
+library(furrr)
+
+#### Settings ####
+
+## Priors, likelihoods
+
+# prior: beta ~ Normal(0,1)
+log_prior <- function(beta){
+  
+  sum( dnorm(beta, mean = rep(0, times = 5), sd = 5, log = T) )
+  
+}
+
+draw_prior <- function(n){
+  matrix(
+    rnorm(n * 5, mean = rep(0, times = 5), sd = 5), ncol = 5, byrow = T
+  )
+}
+
+log_like <- function(beta, X, y){
+  Sys.sleep(0.1)
+  sum( dnorm(y, mean = X %*% beta, sd = 1, log = T) )
+  
+}
+
+log_like_approx <- function(beta, X, y, bias){
+  
+  sum( dnorm(y, mean = X %*% beta + bias, sd = 1.5, log = T) )
+  
+}
+
+nn_optimise_pre_approx_llhood_transformation <- function(particles, b_s_start, loglike, temp){
+  
+  #log_theta is matrix
+  # should only send values of log_like that have been cached already
+  true_post <- apply(particles, MARGIN = 1, loglike, type = "full_posterior", temp = temp)
+  
+  len <- ncol(particles)
+  
+  if(missing(b_s_start)){
+    
+    approx_post <-apply(particles, MARGIN = 1, loglike, type = "approx_posterior", temp = temp)
+    mode_true <- which.max(true_post)
+    mode_approx <- which.max(approx_post)
+    b_s_start <- rep(0, times = 2 * len)
+    b_s_start[1:len] <- particles[mode_approx,] - particles[mode_true,]
+  }
+  
+  topt <- function(b_s){
+    
+    b_mat <- matrix(b_s[1:len], ncol = len, nrow = nrow(particles), byrow = T)
+    s_mat <- matrix(b_s[(len+1):(2*len)], ncol = len, nrow = nrow(particles), byrow = T)
+    
+    approx_post <- apply((particles - b_mat)/exp(s_mat), MARGIN = 1, loglike, type = "approx_posterior", temp = temp)
+    
+    keep_ll <- is.finite(approx_post)
+    
+    # Chi-sq discrepancy
+    sum( # p * (p/q - 1)^2
+      ( exp(approx_post[keep_ll] - 0.5 * true_post[keep_ll] ) - exp(0.5 * true_post[keep_ll]) )^2
+    )
+    
+  }
+  
+  optim(par = b_s_start, fn = topt, control = list(maxit = 1), method = "BFGS")
+  
+}
+
+nn_posterior <- function(y, X, sigma, tau){
+  p <- ncol(X)
+  n <- length(y)
+  XtX <- crossprod(X)
+  Xty <- crossprod(X,y)
+  mu <- solve(XtX + diag(1/(tau^2), nrow = p), Xty)
+  V <- solve(XtX + diag(1/(tau^2), nrow = p)) * (sigma^2)
+  
+  log_z_lhood <- -(n/2) * ( log(2) + log(pi) + 2 * log(sigma) )
+  log_z_prior <- -(p/2) * ( log(2) + log(pi) + 2 * log(tau) )
+  log_z_post <- -(p/2) * ( (log(2) + log(pi)) + log(det(V)) )
+  
+  return(list(
+    mu = mu,
+    var = V,
+    log_z =  log_z_prior + log_z_lhood - log_z_post
+  ))
+  
+}
+
+# true_theta = (birth rate prey, death rate prey/ birth rate pred, death rate pred)
+g_pars <- list(N = 100, N_approx = 100, true_beta = c(0, 0.5, -1.5, 1.5, -3),
+               log_prior = log_prior, log_like = log_like, log_like_approx = log_like_approx,
+               draw_prior = draw_prior, optimise_pre_approx_llhood_transformation = nn_optimise_pre_approx_llhood_transformation
+)
+
+sim_settings <- rep(list(list(f_pars = NULL, g_pars = g_pars)), 2)
+
+sim_settings[[1]]$f_pars <- list(
+  num_p = 100,
+  step_scale_set = c(0.05, 0.1, 0.2, 0.4, 0.6),
+  b_s_start = rep(0, 10),
+  refresh_ejd_threshold = 1,
+  approx_ll_bias = 1
+)
+
+sim_settings[[2]]$f_pars <- list(
+  num_p = 200,
+  step_scale_set =  c(0.05, 0.1, 0.2, 0.4, 0.6),
+  b_s_start = rep(0, 10),
+  refresh_ejd_threshold = 1,
+  approx_ll_bias = 1
+)
+
+simulate_regr <- function(N, beta){
+  
+  X <- matrix(rnorm(length(beta)*N), nrow = N)
+  y <- X %*% beta + rnorm(N)
+  
+  return(
+    list(y = y, X = X, beta = beta)
+  )
+  
+}
+
+####
+
+#### Code for sim ####
+
+run_sim <- function(ss, verbose = F){
+  
+  ## simulate data
+  
+  sim <- simulate_regr(N = ss$g_pars$N, beta = ss$g_pars$true_beta)
+  
+  source("nn-smc-da.R")
+  
+  log_like <- function(beta) ss$g_pars$log_like(beta, X = sim$X, y = sim$y)
+  log_like_approx <- function(beta) ss$g_pars$log_like_approx(beta, X = sim$X[1:ss$g_pars$N_approx,], 
+                                                              y = sim$y[1:ss$g_pars$N_approx], 
+                                                              bias = ss$f_pars$approx_ll_bias)
+  
+  ## run
+  cat("smc da")
+  smc_da <- with(ss$f_pars, run_smc_da(
+    use_da = T, use_approx = F,
+    num_p = num_p,
+    step_scale_set = step_scale_set,
+    b_s_start = b_s_start,
+    refresh_ejd_threshold = refresh_ejd_threshold,
+    log_prior = ss$g_pars$log_prior,
+    log_like = log_like,
+    log_like_approx = log_like_approx,
+    draw_prior = ss$g_pars$draw_prior,
+    optimise_pre_approx_llhood_transformation =  ss$g_pars$optimise_pre_approx_llhood_transformation,
+    verbose = verbose
+  )
+  )
+  cat("smc full")
+  smc_full <- with(ss$f_pars, run_smc_da(
+    use_da = F, use_approx = F,
+    num_p = num_p,
+    step_scale_set = step_scale_set,
+    b_s_start = b_s_start,
+    refresh_ejd_threshold = refresh_ejd_threshold,
+    log_prior = ss$g_pars$log_prior,
+    log_like = log_like,
+    log_like_approx = log_like_approx,
+    draw_prior = ss$g_pars$draw_prior,
+    optimise_pre_approx_llhood_transformation =  ss$g_pars$optimise_pre_approx_llhood_transformation,
+    verbose = verbose
+  )
+   )
+  
+  # cat("smc approx")
+  # smc_approx <- with(ss$f_pars, smc_lotka_volterra_da(
+  #   use_da = F, use_approx = T,
+  #   num_p = num_p,
+  #   step_scale_set = step_scale_set,
+  #   b_s_start = b_s_start,
+  #   refresh_ejd_threshold = refresh_ejd_threshold,
+  #   log_prior = log_prior,
+  #   log_like = log_like,
+  #   log_like_approx = log_like_approx,
+  #   verbose = verbose
+  # )
+  # )
+  
+  
+  return(
+    list(sim_settings = ss,
+         sim_data = sim_data,
+         smc_da = smc_da,
+         smc_full = smc_full,
+         smc_approx = smc_approx
+    )
+  )
+  
+}
+
+####
+
+res <- run_sim(ss = sim_settings[[1]], verbose = T)
+
+#res <- future_map(1:100, .f = function(x) , .progress = T)
