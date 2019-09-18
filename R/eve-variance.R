94d8d578867b3f75ee8e923d3e6a7e0bbaba5b95
bonStats <joshuajbon@gmail.com>
2019-09-18 11:35:20 +1000

    Eve estimator updated

--- R/eve-variance.R
+++ R/eve-variance.R
@@ -1,20 +1,24 @@
 #' Variance estimator using ancestory
 #'
+#' See: Supplementary material for 'Variance estimation in the particle filter'
+#'
 #' @param particles Particles to calculate variance estimate for.
 #' @param log_z log of normalising constant estimated by algorithm.
 #' @param num_iter Number of SMC iterations.
 #'
-#' @return estimate of MC variance.
+#' @return estimate of MC variance of normalising constant.
 #' @export
 #'
 eve_var_est <- function(particles, log_z, num_iter){
 
   N <- num_particles(particles)
-  unq_eve <- length(unique(eve(particles)))
 
+  Z_sq <- exp(2 * log_z)
 
-  Vn <- 1 - exp(num_iter * (log(N) - log(N-1)) - 2 * log(N) + log(2 * unq_eve))
+  S_0_n <- table(factor(eve(particles), levels = 1:N)) / N
 
-  exp(2 * log_z) * Vn
+  m_star <- exp(num_iter * ( log(N) - log(N-1) )) * ( Z_sq - sum( S_0_n^2 ) )
+
+  Z_sq - m_star
 
 }
