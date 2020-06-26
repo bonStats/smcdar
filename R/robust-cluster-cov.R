1b14571e4c31701348bbf3f8ab6ad067bbb00ee4
Joshua Bon <joshuajbon@gmail.com>
2020-06-26 14:04:54 +1000

    Robust cov and docs

---
+++ R/robust-cluster-cov.R
@@ -0,0 +1,37 @@
+#' Robust covariance and correlation estimation from matrix
+#'
+#' Uses a clustering algorithm to estimate means, then estimate the covariance or correlation matrix.
+#' Better for multimodal distributions.
+#'
+#' @param X A matrix, rows are observations.
+#' @param ncluster Number of clusters to use. If missing \code{ClusterR::Optimal_Clusters_KMeans} is used.
+#' @param ... Settings for \code{stats::cov.wt}.
+#'
+#' @return Covariance or correlation matrix.
+#' @export
+#'
+robust_mean_cov <- function(X, ncluster, ...){
+
+  if(missing(ncluster)){
+    opt_cluster <- ClusterR::Optimal_Clusters_KMeans(
+      X,
+      max_clusters = 5,
+      criterion = "BIC",
+      num_init = 10,
+      plot_clusters = F)
+
+    ncluster <- which.min(opt_cluster)
+  }
+
+  opt_kmeans <- kmeans(X, centers = ncluster)
+  X_centered <- X - opt_kmeans$centers[opt_kmeans$cluster,]
+
+  cv <- cov.wt(X_centered, ...)
+
+  if(exists("cor", where =  cv)){
+    return(cv$cor)
+  } else {
+    return(cv$cov)
+  }
+
+}
